7033f6a91494394c1d7756cc9cb139361ac9fb9e
Chiranjib Biswal <106870264+GLCHIRANJIB@users.noreply.github.com>
2022-07-31 23:22:27 +0530

    Add files via upload

---
+++ graded project2/Construction_Project/src/com/cb/construction/Main.java
@@ -0,0 +1,12 @@
+package com.cb.construction;
+
+public class Main {
+	 public static int totalFloors;
+	    public static int[] totalFloorSize;
+
+	    public static void main(String[] args) {
+	        Driver myDriverObj = new Driver();
+	        myDriverObj.getInputs();
+	        myDriverObj.calculateAndPrintOrderOfConstruction(myDriverObj.totalFloorSize);
+	    }
+	} 
